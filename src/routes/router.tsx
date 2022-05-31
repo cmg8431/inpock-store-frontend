a7616e36dd7de9ae142003fd6fc3f53738f79587
최민기 <cmg8431@gmail.com>
2022-05-31 22:44:50 +0900

    feat: main 페이지 router에 추가

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { MainPage } from "src/pages";
 
 export const AppRouter: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<>Hello, React</>} />
+        <Route index element={<MainPage />} />
       </Routes>
     </BrowserRouter>
   );
