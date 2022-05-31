9b4b1f4e6d5019cb7948331d57c56d8bd1d54484
최민기 <cmg8431@gmail.com>
2022-05-31 22:42:13 +0900

    feat: reset에서 normalize로 변경

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -1,12 +1,14 @@
 import { createGlobalStyle } from "styled-components";
-import { reset } from "styled-reset";
+import { normalize } from "styled-normalize";
 
 export const GlobalStyle = createGlobalStyle`
-    ${reset}
+    ${normalize}
+    
     * {
         box-sizing: border-box;
+        font-family: 'Spoqa Han Sans Neo', sans-serif;
     }
-    
+
     html, body, #app {
         width: 100%;
         height: 100%;
