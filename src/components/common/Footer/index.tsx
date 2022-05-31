b78f85f9753e9a7cc5df2c60dc2f8cec7d86ca29
최민기 <cmg8431@gmail.com>
2022-05-31 21:44:29 +0900

    feat: 메인 페이지 Footer 기본 세팅

---
+++ src/components/common/Footer/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import * as S from "./styled";
+
+export const Footer: React.FC = () => {
+  return (
+    <S.Wrapper>
+      <h1>Footer</h1>
+    </S.Wrapper>
+  );
+};
---
+++ src/components/common/Footer/styled.ts
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  background-color: rgb(246, 246, 246);
+`;
