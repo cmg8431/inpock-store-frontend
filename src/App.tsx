8038dc95933baab95638df0fdef2af10e2fb82f9
최민기 <cmg8431@gmail.com>
2022-06-17 15:39:17 +0900

    chore: Query 세팅, Recoil 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import React from "react";
 import { GlobalStyle } from "src/styles";
 import { AppRouter } from "src/routes";
+import { RecoilRoot } from "recoil";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 export const App: React.FC = () => {
   return (
-    <>
-      <GlobalStyle />
-      <AppRouter />
-    </>
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <GlobalStyle />
+        <AppRouter />
+      </QueryClientProvider>
+    </RecoilRoot>
   );
 };
