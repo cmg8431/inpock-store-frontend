6dbb27af1ca21db550f12d6061288145c4f95453
최민기 <cmg8431@gmail.com>
2022-06-24 13:41:36 +0900

    feat: TextField info 추가

--- src/components/common/TextField/index.tsx
+++ src/components/common/TextField/index.tsx
@@ -1,22 +1,24 @@
 /* eslint-disable react/jsx-props-no-spreading */
-/* eslint-disable react/self-closing-comp */
+/* eslint-disable react/require-default-props */
 import React, { InputHTMLAttributes } from "react";
 import * as S from "./styled";
 import ImportantSVG from "src/assets/svg/Important.svg";
 
 interface TextFieldProps extends InputHTMLAttributes<HTMLInputElement> {
-  info: string;
-  important: boolean;
+  info?: string;
+  important?: boolean;
 }
 
 export const TextField = React.forwardRef<HTMLInputElement, TextFieldProps>(
   ({ important = false, info, ...options }, ref) => {
     return (
       <>
-        <S.Label>
-          <span style={{ marginRight: "0.2rem" }}>{info}</span>
-          {important ? <ImportantSVG /> : null}
-        </S.Label>
+        {info && (
+          <S.Label>
+            <span style={{ marginRight: "0.2rem" }}>{info}</span>
+            {important && <ImportantSVG />}
+          </S.Label>
+        )}
         <S.TextField ref={ref} {...options} />
       </>
     );
