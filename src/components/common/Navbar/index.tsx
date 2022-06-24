ea1d461a27d11d6925495243047100ae372c2f93
최민기 <cmg8431@gmail.com>
2022-06-24 13:43:26 +0900

    feat: link 추가

--- src/components/common/Navbar/index.tsx
+++ src/components/common/Navbar/index.tsx
@@ -3,13 +3,18 @@ import * as S from "./styled";
 import LogoPNG from "src/assets/png/Logo.png";
 import UserSVG from "src/assets/svg/User.svg";
 import ShoppingBagSVG from "src/assets/svg/ShoppingBag.svg";
+import { Link } from "react-router-dom";
 
 export const Navbar: React.FC = () => {
   return (
     <S.NavbarWrapper>
-      <S.NavbarImg src={LogoPNG} alt="INPOCK" />
+      <Link to="/">
+        <S.NavbarImg src={LogoPNG} alt="INPOCK" />
+      </Link>
       <div>
-        <UserSVG />
+        <Link to="/mypage">
+          <UserSVG />
+        </Link>
         <ShoppingBagSVG style={{ marginLeft: "1rem" }} />
       </div>
     </S.NavbarWrapper>
