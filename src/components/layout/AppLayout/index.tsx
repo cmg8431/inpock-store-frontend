6d3fca3dbf62ca1417158691833a3482aeee079b
최민기 <cmg8431@gmail.com>
2022-06-24 13:48:23 +0900

    feat: applayout 추가

--- src/components/layout/AppLayout/index.tsx
+++ src/components/layout/AppLayout/index.tsx
@@ -10,12 +10,23 @@ interface AppLayoutProps {
 }
 
 export const AppLayout: React.FC<AppLayoutProps> = ({ children, title, src }) => {
+  const indentTitle = title.split("/");
+
   return (
     <S.AppLayoutWrapper>
       <Link to={src}>
         <ArrowRightSVG />
       </Link>
-      <S.AppLayoutTitle>{title}</S.AppLayoutTitle>
+
+      {indentTitle.length >= 1 ? (
+        <div style={{ margin: "1.5rem 0" }}>
+          {indentTitle.map((title) => {
+            return <S.AppLayoutTitle style={{ margin: "0.5rem 0" }}>{title}</S.AppLayoutTitle>;
+          })}
+        </div>
+      ) : (
+        <S.AppLayoutTitle>{title}</S.AppLayoutTitle>
+      )}
       <S.AppLayoutChildrenWrapper>{children}</S.AppLayoutChildrenWrapper>
     </S.AppLayoutWrapper>
   );
