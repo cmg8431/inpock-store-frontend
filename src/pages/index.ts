afeecefd062d8b89a2af26d209af781b11bb476b
최민기 <cmg8431@gmail.com>
2022-06-24 13:58:17 +0900

    feat: page export 및 router 설정

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,1 +1,4 @@
 export * from "./main";
+export * from "./auth";
+export * from "./mypage";
+export * from "./userinfo";
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,12 +1,22 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { MainPage } from "src/pages";
+import { LoginPage, MainPage, UserInfoPage, MyPage, TelCheckPage, SignUpPage, AgreePage } from "src/pages";
 
 export const AppRouter: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<MainPage />} />
+        <Route path="mypage" element={<MyPage />} />
+        <Route path="mypage/userinfo" element={<UserInfoPage />} />
+        <Route path="auth">
+          <Route path="login" element={<LoginPage />} />
+          <Route path="register">
+            <Route path="agree" element={<AgreePage />} />
+            <Route path="signup" element={<SignUpPage />} />
+            <Route path="telcheck" element={<TelCheckPage />} />
+          </Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   );
