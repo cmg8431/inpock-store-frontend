a9208ac5a63ced8ff212c48ef5d4ce69dbb76fcc
최민기 <cmg8431@gmail.com>
2022-06-24 13:50:10 +0900

    feat: auth login page 구현

---
+++ src/pages/auth/login/index.tsx
@@ -0,0 +1,68 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from "react";
+import * as S from "./styled";
+import { AppLayout, CommonButton, TextField, Toast } from "src/components";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { useLogin } from "src/hook/query";
+import { LoginFormValues } from "src/api/user";
+
+export const LoginPage: React.FC = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginFormValues>();
+  const navigate = useNavigate();
+  const { isLoading, mutate } = useLogin();
+  const onSubmit = ({ username, password }: LoginFormValues) => {
+    mutate({ username, password });
+  };
+
+  return (
+    <AppLayout title="로그인" src="/mypage">
+      <Toast />
+      <S.LoginForm onSubmit={handleSubmit(onSubmit)}>
+        <TextField
+          important
+          info="아이디"
+          type="text"
+          placeholder="아이디를 입력해주세요."
+          style={{ marginBottom: "1rem" }}
+          {...register("username", {
+            required: "필수 응답 항목입니다.",
+          })}
+        />
+        {errors.username?.message}
+        <TextField
+          important
+          info="비밀번호"
+          type="password"
+          placeholder="비밀번호를 입력해주세요."
+          {...register("password", {
+            required: "필수 응답 항목입니다.",
+            minLength: {
+              value: 3,
+              message: "비밀번호는 8자 이상이여야 합니다,",
+            },
+          })}
+        />
+        {errors.password?.message}
+        <S.LoginAuthFindWrapper>
+          <S.LoginGoToLink to="/auth/find">아이디 / 비밀번호 찾기</S.LoginGoToLink>
+        </S.LoginAuthFindWrapper>
+        <CommonButton styleType="default" type="submit">
+          {isLoading ? <span>로딩중</span> : <span>로그인</span>}
+        </CommonButton>
+        <CommonButton styleType="orangeBorder" onClick={() => navigate("/auth/register/agree")}>
+          3초만에 빠른 회원가입
+        </CommonButton>
+        <S.LoginOrderInquiry>
+          <S.LoginGoToLink style={{ marginTop: "1rem" }} to="/">
+            비회원 주문 조회
+          </S.LoginGoToLink>
+        </S.LoginOrderInquiry>
+      </S.LoginForm>
+    </AppLayout>
+  );
+};
---
+++ src/pages/auth/login/styled.ts
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export const LoginForm = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const LoginSubTitle = styled.p`
+  margin: 0rem;
+  letter-spacing: -0.01rem;
+  font-size: 0.9rem;
+`;
+
+export const LoginGoToLink = styled(Link)`
+  margin-left: 0.2rem;
+  color: #343434;
+  font-size: 0.85rem;
+  text-underline-position: under;
+`;
+
+export const LoginAuthFindWrapper = styled.p`
+  display: flex;
+  font-size: 0.9rem;
+  font-weight: 400;
+  letter-spacing: -0.03rem;
+  justify-content: flex-end;
+`;
+
+export const LoginOrderInquiry = styled.p`
+  display: flex;
+  justify-content: center;
+`;
