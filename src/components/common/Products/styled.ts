76312240a2f4315adaa0bb72944e1d5d24423568
최민기 <cmg8431@gmail.com>
2022-06-24 13:45:41 +0900

    feat: products margin-bottom 스타일 추가

--- src/components/common/Products/styled.ts
+++ src/components/common/Products/styled.ts
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const ProductsWrapper = styled.div`
   padding-left: 16px;
+  margin-bottom: 1rem;
 `;
 
 export const ProductsContentWrapper = styled.span`
