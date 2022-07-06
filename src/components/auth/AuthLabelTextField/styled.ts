4b1fc8c99889ce74abc436b9a17a509fc106a05b
최민기 <cmg8431@gmail.com>
2022-07-06 19:43:12 +0900

    feat: auth 컴포넌트 추가

---
+++ src/components/auth/AuthLabelTextField/styled.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const AuthLabelTextFieldContainer = styled.div``;
+
+export const AuthLabelTextFieldTextContainer = styled.label`
+  font-size: 0.88rem;
+  font-weight: 400;
+  color: #343434;
+  margin: 1.2rem 0rem 1rem 0rem;
+  display: flex;
+  align-items: center;
+
+  & > span {
+    margin-right: 0.5rem;
+  }
+`;
